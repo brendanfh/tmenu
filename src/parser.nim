a72072508a73cab09b1557c13c50c57eebf94cde
Brendan Hansen <brendan.f.hansen@gmail.com>
2019-10-22 11:08:11 -0500

    Changed parser slightly

--- src/parser.nim
+++ src/parser.nim
@@ -1,6 +1,20 @@
 import docopt
-import strutils
 import os
+import strutils
+
+type
+  ParserArgs = ref object
+    t: string
+    start_dir: string
+    height: uint
+    layout: string
+    incremental: bool
+    exact: bool
+    sort: bool
+    case_sensitive: bool
+    multi_select: bool
+    preview: string
+    input: string
 
 let doc = """
 tmenu
@@ -22,41 +36,56 @@ Options:
 
 """
 
-let args = docopt(doc, version="tmenu 0.1")
+proc readArgs*(): ParserArgs =
+  let args = docopt(doc, version="tmenu 0.1")
+  var
+    t = "default"
+    start_dir = getCurrentDir()
+    height = 0 as uint
+    layout = "default"
+    incremental = args["--incremental"]
+    exact = args["--exact"]
+    sort = not args["--no-sort"] and not args["+s"]
+    case_sensitive = args["+i"]
+    multi_select = args["--multi"] or args["--multi-select"]
+    preview = if args["--preview"]: $args["--preview"] else: ""
+    input = ""
 
-var
-  t = "default"
-  start_dir = getCurrentDir()
-  height = 0
-  layout = "default"
-  incremental = args["--incremental"]
-  exact = args["--exact"]
-  sort = not args["--no-sort"] and not args["+s"]
-  case_sensitive = args["+i"]
-  mutli_select = args["--multi"] or args["--multi-select"]
-  preview = if args["--preview"]: $args["--preview"] else: ""
-  input = ""
-
-try:
-  if getFileSize(stdin) > 0:
-    input = readAll(stdin)
-except IOError: # An IOError is thrown when you try to read the input from `echo "asdf" | parser`
-  input = readAll(stdin)
-
-if args["--type"]:
-  doAssert $args["--type"] in ["default", "d", "dir", "f", "file"]
-  t = $args["--type"]
-if args["--start-dir"]:
-  doAssert existsDir expandTilde $args["--start-dir"]
-  start_dir = $args["--start-dir"]
-if args["--height"]:
   try:
-    height = parseInt($args["--height"])
-  except ValueError:
-    echo "--height=<h> must be a integer"
-    quit(QuitFailure)
-if args["--layout"]:
-  doAssert $args["--layout"] in ["default", "reverse", "reverse-list"]
-  layout = $args["--layout"]
+    if getFileSize(stdin) > 0:
+      input = readAll(stdin)
+  except IOError: # An IOError is thrown when you try to read the input from `echo "asdf" | parser`
+    input = readAll(stdin)
 
-echo "type: ", t, "\nstart_dir: ", start_dir, "\nheight: ", height, "\nlayout: ", layout, "\nincremental: ", incremental, "\nexact: ", exact, "\nsort: ", sort, "\ncase_sensitive: ", case_sensitive, "\nmulti_select: ", mutli_select, "\npreview: \"", preview, "\"\nstdin: \"", input, "\""
+  if args["--type"]:
+    doAssert $args["--type"] in ["default", "d", "dir", "f", "file"]
+    t = $args["--type"]
+  if args["--start-dir"]:
+    doAssert existsDir expandTilde $args["--start-dir"]
+    start_dir = $args["--start-dir"]
+  if args["--height"]:
+    try:
+      height = parseInt($args["--height"])
+    except ValueError:
+      echo "--height=<h> must be a integer"
+      quit(QuitFailure)
+  if args["--layout"]:
+    doAssert $args["--layout"] in ["default", "reverse", "reverse-list"]
+    layout = $args["--layout"]
+
+  return ParserArgs(
+    t: t,
+    start_dir: start_dir,
+    height: height,
+    layout: layout,
+    incremental: incremental,
+    exact: exact,
+    sort: sort,
+    incremental: incremental,
+    exact: exact,
+    sort: sort,
+    case_sensitive: case_sensitive,
+    multi_select: multi_select,
+    preview: preview,
+    input: input
+  )
--- src/tmenu.nim
+++ src/tmenu.nim
@@ -1,37 +1,41 @@
 import os, strutils
 import illwill
 
+import parser
+
 when isMainModule:
-  proc exitProc() {.noconv.} =
-    illwillDeinit()
-    showCursor()
-    quit(0)
+  var args = readArgs()
 
-  illwillInit(fullscreen=true)
-  setControlCHook(exitProc)
-  hideCursor()
-
-  var tb = newTerminalBuffer(terminalWidth(), terminalHeight())
-  tb.setForegroundColor(fgBlack, true)
-  tb.drawRect(0, 0, 40, 5)
-  tb.drawHorizLine(2, 38, 3, doubleStyle=true)
-
-  tb.write(2, 1, fgWhite, "Press any key to display its name")
-  tb.write(2, 2, "Press ", fgYellow, "ESC", fgWhite, " or ", fgYellow, "Q", fgWhite, " to quit")
-
-  while true:
-    var bb = newBoxBuffer(tb.width, tb.height)
-    bb.draw_rect(0, 0, tb.width - 1, 5)
-    tb.write(fgWhite)
-    tb.write(bb)
-
-    var key = getKey()
-    case key
-    of Key.None: discard
-    of Key.Escape, Key.Q: exitProc()
-    else:
-      tb.write(8, 4, ' '.repeat(31))
-      tb.write(2, 4, resetStyle, "Key pressed: ", fgGreen, $key)
-
-    tb.display()
-    sleep(20)
+#  proc exitProc() {.noconv.} =
+#    illwillDeinit()
+#    showCursor()
+#    quit(0)
+#
+#  illwillInit(fullscreen=true)
+#  setControlCHook(exitProc)
+#  hideCursor()
+#
+#  var tb = newTerminalBuffer(terminalWidth(), terminalHeight())
+#  tb.setForegroundColor(fgBlack, true)
+#  tb.drawRect(0, 0, 40, 5)
+#  tb.drawHorizLine(2, 38, 3, doubleStyle=true)
+#
+#  tb.write(2, 1, fgWhite, "Press any key to display its name")
+#  tb.write(2, 2, "Press ", fgYellow, "ESC", fgWhite, " or ", fgYellow, "Q", fgWhite, " to quit")
+#
+#  while true:
+#    var bb = newBoxBuffer(tb.width, tb.height)
+#    bb.draw_rect(0, 0, tb.width - 1, 5)
+#    tb.write(fgWhite)
+#    tb.write(bb)
+#
+#    var key = getKey()
+#    case key
+#    of Key.None: discard
+#    of Key.Escape, Key.Q: exitProc()
+#    else:
+#      tb.write(8, 4, ' '.repeat(31))
+#      tb.write(2, 4, resetStyle, "Key pressed: ", fgGreen, $key)
+#
+#    tb.display()
+#    sleep(20)
